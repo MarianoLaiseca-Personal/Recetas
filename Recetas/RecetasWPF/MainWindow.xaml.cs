9cf2d6fac85bd55d67da44dc1bc70a6d0de99f0d
Mariano Laiseca <mlaiseca@users.noreply.github.com>
2020-08-27 14:54:14 -0300

    Primer Ejemplo con clases principales

--- Recetas/RecetasWPF/MainWindow.xaml.cs
+++ Recetas/RecetasWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RecetasWPF.Models;
 
 namespace RecetasWPF
 {
@@ -23,6 +25,116 @@ namespace RecetasWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UnidadDeMedida_Tipo ut1 = new UnidadDeMedida_Tipo()
+            {
+                ID = 1,
+                Descripcion = "Peso" };
+            UnidadDeMedida_Tipo ut2 = new UnidadDeMedida_Tipo()
+            {
+                ID = 2,
+                Descripcion = "Volumen" };
+
+            UnidadDeMedida um1 = new UnidadDeMedida()
+            {
+                ID = 1,
+                Descripcion = "Gramos",
+                Abreviatura = "gr",
+                umt = ut1,
+                UnidadDeMedida_TipoID = ut1.ID };
+            UnidadDeMedida um2 = new UnidadDeMedida()
+            {
+                ID = 2,
+                Descripcion = "Mililitros",
+                Abreviatura = "ml",
+                umt = ut2,
+                UnidadDeMedida_TipoID = ut2.ID };
+
+            Ingrediente ing1 = new Ingrediente()
+            {
+                ID = 1,
+                Descripcion = "Leche",
+                UnidadDeMedidaID = um2.ID,
+                UM_Base = um2
+            };
+
+            Ingrediente ing2 = new Ingrediente()
+            {
+                ID = 2,
+                Descripcion = "Azucar",
+                UnidadDeMedidaID = um1.ID,
+                UM_Base = um1
+            };
+
+            Ingrediente ing3 = new Ingrediente()
+            {
+                ID = 3,
+                Descripcion = "Arroz",
+                UnidadDeMedidaID = um1.ID,
+                UM_Base = um1
+            };
+
+            Ingrediente ing4 = new Ingrediente()
+            {
+                ID = 4,
+                Descripcion = "Azucar Impalpable",
+                UnidadDeMedidaID = um1.ID,
+                UM_Base = um1
+            };
+
+            Receta rec1 = new Receta()
+            {
+                ID = 1,
+                Descripcion = "Dulce de Leche",
+                UnidadDeMedidaID = um1.ID,
+                UM_Base = um1
+            };
+            rec1.AgregarComponente(new Componente()
+            {
+                ID = 1,
+                Comp = ing1,
+                CantidadNecesaria = 100,
+                UnidadDeMedidaID = um2.ID,
+                UM = um2
+            });
+            rec1.AgregarComponente(new Componente()
+            {
+                ID = 2,
+                Comp = ing2,
+                CantidadNecesaria = 200,
+                UnidadDeMedidaID = um2.ID,
+                UM = um2
+            });
+            Debug.WriteLine(rec1.Mostrar());
+
+            /*
+            Receta rec2 = new Receta()
+            {
+                ID = 2,
+                Descripcion = "Arroz con Leche",
+                UnidadDeMedidaID = um1.ID,
+                UM_Base = um1
+            };
+
+            Receta rec3 = new Receta()
+            {
+                ID = 3,
+                Descripcion = "Arroz con Leche con Dulce de Leche",
+                UnidadDeMedidaID = um1.ID,
+                UM_Base = um1
+            };
+
+            Receta rec4 = new Receta()
+            {
+                ID = 4,
+                Descripcion = "Vauquita Casera",
+                UnidadDeMedidaID = um1.ID,
+                UM_Base = um1
+            };
+            */            
         }
     }
 }
--- Recetas/RecetasWPF/Models/UnidadDeMedida.cs
+++ Recetas/RecetasWPF/Models/UnidadDeMedida.cs
@@ -10,6 +10,7 @@ namespace RecetasWPF.Models
     {
         public int ID { get; set; }
         public string Descripcion { get; set; }
+        public string Abreviatura { get; set; }
         public int UnidadDeMedida_TipoID { get; set; }
         public UnidadDeMedida_Tipo umt { get; set; }
     }
