630ffc0c2cee499e9a999fc59fbb1ec0210a09c3
marianolaiseca <mlaiseca@users.noreply.github.com>
2020-09-02 17:32:33 -0300

    Cambio de prueba

--- Recetas/RecetasWPF/Models/RecetasDBContext.cs
+++ Recetas/RecetasWPF/Models/RecetasDBContext.cs
@@ -20,6 +20,7 @@ namespace RecetasWPF.Models
         public DbSet<Receta> Recetas { get; set; }
         public DbSet<Componente> Componentes { get; set; }
 
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
