6377171e12032e341c304aced436681eb1cd86c8
MarianoLaiseca <MarianoLaiseca@users.noreply.github.com>
2020-09-12 20:29:49 -0300

    Agregado nuevo proyecto para utilizar con LiteDB

---
+++ Recetas/LibroDeRecetas/Models/Componente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibroDeRecetas.Models
+{
+    public abstract class Componente
+    {
+        protected string Nombre;
+
+    }
+}
---
+++ Recetas/LibroDeRecetas/Models/UnidadDeMedida.cs
@@ -0,0 +1,17 @@
+﻿namespace LibroDeRecetas.Models
+{
+    public enum TipoUnidadDeMedida
+    {
+        Peso,
+        Volumen,
+        Unidad,
+        Otro
+    }
+
+    public class UnidadDeMedida
+    {
+        public int UnidadDeMedidaID { get; set; }
+        public string Descripcion { get; set; }
+        public TipoUnidadDeMedida TipoUM { get; set; }
+    }
+}
